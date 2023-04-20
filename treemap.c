649537e63c9f0124d377b1eafdeb4b32f0217929
Replit user <>
2023-04-20 17:36:39 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -63,9 +63,15 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
         nuevoNodo->parent = tree->current->right;
         return;
       }
+      
       tree->current = tree->current->right;
     } else {
-      tree->current = tree->current->left;
+      if(tree->current->left == NULL) {
+        tree->current->left = nuevoNodo;
+        nuevoNodo->parent = tree->current->left;
+        return;
+      }
+      //tree->current = tree->current->left;
     }
   }
 
